17b26b79bd25b9d2e68b6e91571f3fc30c9e80be
EC2 Default User <ec2-user@ip-172-31-17-66.us-east-2.compute.internal>
2021-02-22 10:04:17 +0000

    Laravel応用課題11（途中）

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,14 @@ Route::get('/', function () {
 // ユーザ登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+//ログイン機能
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+
+Route::resource('users', 'UsersController', ['only' => ['show']]);
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('movies', 'MoviesController', ['only' => ['create', 'store', 'destroy']]);
+});
