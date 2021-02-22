8cf46d00699c05d98c705cadaeb479d9cd68d6f8
EC2 Default User <ec2-user@ip-172-31-17-66.us-east-2.compute.internal>
2021-02-22 05:23:19 +0000

    Laravel応用課題8~10

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="center jumbotron bg-warning">
+
+        <div class="text-center text-white">
+            <h1>YouTubeまとめ × SNS</h1>
+        </div>
+
+    </div>
+
+@endsection
